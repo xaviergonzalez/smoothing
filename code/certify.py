fixed bug in finding jacobian by hand (for loop)

code/certify.py
@@ -40,8 +40,6 @@ parser.set_defaults(feature=False)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    if args.gpu:
-        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
     # load the base classifier
     checkpoint = torch.load(args.base_classifier)
     base_classifier = get_architecture(checkpoint["arch"], args.dataset, noise_std = args.noise_std_lst, hidden_size = args.hidden_size, nonlinear = args.nonlinear)
@@ -90,14 +88,15 @@ if __name__ == "__main__":
             x.requires_grad_(True)
             mask = (base_classifier(x)[1] > 0).cpu().numpy()
             mask = vf(mask)
-            A = A * mask[:, np.newaxis]
-            R = np.transpose(A) @ np.linalg.inv(A @ np.transpose(A))
+            J = A * mask[:, np.newaxis]
+            R = np.transpose(J) @ np.linalg.inv(J @ np.transpose(J))
             a,b,c = np.linalg.svd(R)
             eig_vals = np.zeros(inp_len)
             eig_vals[:hid_len] = (args.noise_std_lst[1] ** 2) * (b ** 2)
             eig_vals = eig_vals + (args.sigma ** 2)
             radii = np.sqrt(eig_vals)
             Bt = np.mean(eig_vals)
+            print(Bt)
             Bd = gmean(radii)
         prediction, radius, pABar = smoothed_classifier.certify(x, args.N0, args.N, args.alpha, args.batch)
         if args.layered_GNI:

code/sing_check.py
@@ -1,16 +1,19 @@
-""" This script loads a base classifier and then runs PREDICT on perturbations of a single example from the dataset to check the returned radius of robustness for that single example
-"""
+# Checks the robustness for a single case, for the minimum radius
 import argparse
+import os
 from datasets import get_dataset, DATASETS, get_num_classes
 from core import Smooth
 from time import time
 import torch
-from architectures import get_architecture
 import datetime
-
+from architectures import get_architecture
+from scipy.stats import norm, binom_test
+import numpy as np
 import pandas as pd
 
-parser = argparse.ArgumentParser(description='Predict on many examples')
+from utils import convert_to_relu
+
+parser = argparse.ArgumentParser(description='Check the coordinates of a certified example')
 parser.add_argument("dataset", choices=DATASETS, help="which dataset")
 parser.add_argument("base_classifier", type=str, help="path to saved pytorch model of base classifier")
 parser.add_argument("sigma", type=float, help="noise hyperparameter")
@@ -18,37 +21,18 @@ parser.add_argument("results", type = str, help = "path to ceritfy output we wan
 parser.add_argument("outfile", type=str, help="output file")
 parser.add_argument("--batch", type=int, default=1000, help="batch size")
 parser.add_argument("--split", choices=["train", "test"], default="test", help="train or test set")
-parser.add_argument("--N", type=int, default=10000, help="number of samples to use")
+parser.add_argument("--N0", type=int, default=10000)
 parser.add_argument("--alpha", type=float, default=0.001, help="failure probability")
 parser.add_argument("--hidden_size", type=int, default=444, help="hidden size of mlp")
 parser.add_argument('--nonlinear', default=0, type=int,
                     help="is the first hidden layer linear or non-linear")
+parser.add_argument('--ID', default=0, type=int,
+                    help="which example to check")
 parser.add_argument('--noise_std_lst', nargs = '+', type = float, default=[], help='noise for each layer')
 args = parser.parse_args()
 
-def check_robust(classifier, x, rad, label):
-    """
-    Checks manually whether the prediction of a classifier on an input x is robust within a given radius
-    """
-    robust = 1
-    before_time = time()
-    for i in range(len(x)):
-
-        y = x.clone()
-        y[i] = x[i] + rad
-
-        # make the prediction
-        prediction = classifier.predict(y, args.N, args.alpha, args.batch)
-
-        correct = int(prediction == label)
-        robust = robust * correct
-    after_time = time()
-    time_elapsed = str(datetime.timedelta(seconds=(after_time - before_time)))
-    return robust, time_elapsed
-    
-    
-
 if __name__ == "__main__":
+    
     # load the output of certify we are checking
     df = pd.read_csv(args.results, delimiter="\t")
     
@@ -57,23 +41,30 @@ if __name__ == "__main__":
     base_classifier = get_architecture(checkpoint["arch"], args.dataset, noise_std = args.noise_std_lst, hidden_size = args.hidden_size, nonlinear = args.nonlinear)
     base_classifier.load_state_dict(checkpoint['state_dict'])
 
-    # create the smoothed classifier g
+    # create the smooothed classifier g
     smoothed_classifier = Smooth(base_classifier, get_num_classes(args.dataset), args.sigma)
 
     # prepare output file
     f = open(args.outfile, 'w')
-    print("idx\tlabel\tradius\trobust\ttime", file=f, flush=True)
+    print("coord\tlabel\tpredict\trobust\ttime", file=f, flush=True)
 
-    # iterate through the output of certify we are checking
+    # iterate through the single example
     dataset = get_dataset(args.dataset, args.split)
     
-    for i, row in df.iterrows():
-        if row['correct']:
-            index = row['idx']
-            rad = row['radius']
-            (x, label) = dataset[index]
-            x = x.cuda()
-            rob, time_elapsed = check_robust(smoothed_classifier, x, rad, label)
-            # log the prediction and whether it was correct
-            print("{}\t{}\t{:.3}\t{}\t{}".format(index, label, rad, rob, time_elapsed), file=f, flush=True)
+    ID = args.ID
+    rad = (df.loc[df['idx'] == ID]['radius'].values[0])
+    
+    (x, label) = dataset[ID]
+    x = x.cuda()
+    
+    for i in range(len(x)):
+        before_time = time()
+        y = x.clone()
+        y[i] = x[i] + rad
+        # make the prediction
+        prediction = smoothed_classifier.predict(y, args.N0, args.alpha, args.batch)
+        correct = int(prediction == label)
+        after_time = time()
+        time_elapsed = str(datetime.timedelta(seconds=(after_time - before_time)))
+        print("{}\t{}\t{}\t{}\t{}".format(i, label, prediction, correct, time_elapsed), file=f, flush=True)
     f.close()

code/sing_jac_check.py
@@ -1,27 +1,27 @@
-# Checks the robustness returned for multilayer GNI
+# Checks the robustness returned for multilayer GNI, for a single case
 import argparse
 import os
+import setGPU
 from datasets import get_dataset, DATASETS, get_num_classes
 from core import Smooth
 from time import time
 import torch
 import datetime
 from architectures import get_architecture
+from scipy.stats import norm, binom_test
 import numpy as np
 import pandas as pd
 
 from utils import convert_to_relu
 
-parser = argparse.ArgumentParser(description='Certify many examples')
+parser = argparse.ArgumentParser(description='Check the coordinates of a certified example')
 parser.add_argument("dataset", choices=DATASETS, help="which dataset")
 parser.add_argument("base_classifier", type=str, help="path to saved pytorch model of base classifier")
 parser.add_argument("sigma", type=float, help="noise hyperparameter")
-parser.add_argument("results", type = str, help = "path to certify output we want to check")
+parser.add_argument("results", type = str, help = "path to ceritfy output we want to check")
 parser.add_argument("outfile", type=str, help="output file")
 parser.add_argument("--batch", type=int, default=1000, help="batch size")
 parser.add_argument("--split", choices=["train", "test"], default="test", help="train or test set")
-parser.add_argument('--gpu', default=None, type=str,
-                    help='id(s) for CUDA_VISIBLE_DEVICES')
 parser.add_argument("--N0", type=int, default=10000)
 parser.add_argument("--alpha", type=float, default=0.001, help="failure probability")
 parser.add_argument("--hidden_size", type=int, default=444, help="hidden size of mlp")
@@ -31,6 +31,7 @@ parser.add_argument('--noise_std_lst', nargs = '+', type = float, default=[], he
 args = parser.parse_args()
 
 if __name__ == "__main__":
+    
     # load the output of certify we are checking
     df = pd.read_csv(args.results, delimiter="\t")
     
@@ -44,7 +45,7 @@ if __name__ == "__main__":
 
     # prepare output file
     f = open(args.outfile, 'w')
-    print("idx\tlabel\tpredict\trobust\ttime", file=f, flush=True)
+    print("coord\tlabel\tpredict\trobust\ttime", file=f, flush=True)
     
     #compute spectrum of pullback noise
     tst_lst = [param.cpu().detach().numpy() for param in base_classifier.parameters()]
@@ -61,35 +62,45 @@ if __name__ == "__main__":
     else:
         vf = np.vectorize(convert_to_relu)
 
-    # iterate through the output of certify we are checking
+    # iterate through the single example
     dataset = get_dataset(args.dataset, args.split)
     
-    for i, row in df.iterrows():
-        if row['correct']:
-            before_time = time()
-            index = row['idx']
-            (x, label) = dataset[index]
-            x = x.cuda()
-            if args.nonlinear:
-                #manually find Jacobian
-                x.requires_grad_(True)
-                mask = (base_classifier(x)[1] > 0).cpu().numpy()
-                mask = vf(mask)
-                J = A * mask[:, np.newaxis]
-                R = np.transpose(J) @ np.linalg.inv(J @ np.transpose(J))
-                a,b,c = np.linalg.svd(R)
-                eig_vals = np.zeros(inp_len)
-                eig_vals[:hid_len] = (args.noise_std_lst[1] ** 2) * (b ** 2)
-                eig_vals = eig_vals + (args.sigma ** 2)
-                radii = np.sqrt(eig_vals)
-                eig_vecs = a
-            pABar = row['pABar']
-            pABar  = pABar - 0.0005 #to try to deal with rounding error
-            idx = row['idx']
-            # certify the prediction of g around x
-            prediction, robust = smoothed_classifier.jac_certify_check(x, label, args.N0, args.alpha, args.batch, pABar, radii, eig_vecs)
-            after_time = time()
-            time_elapsed = str(datetime.timedelta(seconds=(after_time - before_time)))
-            print("{}\t{}\t{}\t{}\t{}".format(
-                idx, label, prediction, robust, time_elapsed), file=f, flush=True)
+    ID = 0
+    
+    (x, label) = dataset[ID]
+    
+    if args.nonlinear:
+        #manually find Jacobian
+        x.requires_grad_(True)
+        mask = (base_classifier(x)[1] > 0).cpu().numpy()
+        mask = vf(mask)
+        J = A * mask[:, np.newaxis]
+        R = np.transpose(J) @ np.linalg.inv(J @ np.transpose(J))
+        a,b,c = np.linalg.svd(R)
+        eig_vals = np.zeros(inp_len)
+        eig_vals[:hid_len] = (args.noise_std_lst[1] ** 2) * (b ** 2)
+        eig_vals = eig_vals + (args.sigma ** 2)
+        radii = np.sqrt(eig_vals)
+        eig_vecs = a
+    
+    pABar = df['pABar'][ID]
+    if pABar == 1.0:
+        pABar = 0.999
+    radii = radii * norm.ppf(pABar)
+    
+    x = x.cpu().numpy()
+    
+    for i in range(len(x)):
+        before_time = time()
+        y = np.copy(x)
+        y = x + radii[i] * eig_vecs[:, i]
+        y = y.astype(np.float32)
+        y = torch.from_numpy(y)
+        y = y.cuda()
+        # make the prediction
+        prediction = smoothed_classifier.predict(y, args.N0, args.alpha, args.batch)
+        correct = int(prediction == label)
+        after_time = time()
+        time_elapsed = str(datetime.timedelta(seconds=(after_time - before_time)))
+        print("{}\t{}\t{}\t{}\t{}".format(i, label, prediction, correct, time_elapsed), file=f, flush=True)
     f.close()